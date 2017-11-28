410e693745f025ec47ff7c97fa34493d6ac947cf
patrick <patrick>
2017-11-28 22:06:44 +0000

    *** empty log message ***

--- testint.m
+++ testint.m
@@ -2,10 +2,10 @@ function I = testint(s,c,beta)
 
 lm = getLatMP(s,c,beta);
 
-I = integral(@(t)fun(t,c),-lm,lm,'AbsTol',1e-6,'RelTol',1e-3);
+I = integral(@(t)fun(t,c),-lm,lm,'AbsTol',1e-9,'RelTol',1e-6);
 
 function y=fun(t,c)
-y = sqrt(c.rt(t).^2+Dctt(c.rt,t).^2);
+y = sqrt(c.rt(t).^2+Dct(c.rt,t).^2);
 end
 
 end
