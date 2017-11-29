fbbc492fa9d2c43b8cee7c57eadc549dadaf328f
patrick <patrick>
2017-11-29 11:49:13 +0000

    *** empty log message ***

--- getLC.m
+++ getLC.m
@@ -1,10 +1,18 @@
 function lc = getLC(s,c)
+% function lc = getLC(s,c)
 
 switch lower(c.type),
+
   case {'d','dip','dipole'},
-	  lc = asin(sqrt(s.dip.B(c.rt(0),sin(0))/s.dip.B(c.rt(c.tmx),c.mx)));
+
+	  lc = asin(sqrt(s.dip.B(c.rt(0),0)/s.dip.B(c.rt(c.tmx),c.mx)));
+
   case {'m','md','mdisc'},
-	  lc = asin(sqrt(s.md.B(c.rt(0),sin(0))/s.md.B(c.rt(c.tmx),c.mx)));
+
+	  lc = asin(sqrt(s.md.B(c.rt(0),0)/s.md.B(c.rt(c.tmx),c.mx)));
+
   otherwise
+
     error('magnetic field type c.type not recognised',c.type)
+
 end
