2ad5a7c0cd6ccdd7736873f191ccabe4fadb0ef4
patrick <patrick>
2017-11-29 11:55:38 +0000

    *** empty log message ***

--- getLatMPd.m
+++ getLatMPd.m
@@ -1,17 +1,21 @@
 function lm = getLatMPd(L,aeq)
 
-
-if 0,
-  % symbolically takes longer time and requires symbolic toolbox
-  M = sym('M','positive');
-  eqn = M.^6 + 3*M*(sin(aeq)).^4 == 4*(sin(aeq)).^4;
-  COS2M = vpasolve(eqn,M,0.5);
-  COSM = double(sqrt(COS2M(2)));
-  lm = acos(COSM);
-end
-
 % numerically
+%tic
 opts = optimset('fzero');
-myfun = @(x,a) cos(x).^6-sin(a)^2.*sqrt(1+3*sin(x).^2);
-lm = fzero(@(x) myfun(x,aeq),pi/4,opts);
+fun = @(x,a) cos(x).^6-sin(a)^2.*sqrt(1+3*sin(x).^2);
+lm = fzero(@(x) fun(x,aeq),pi/4,opts);
+%toc
 
+return
+
+% symbolically takes longer time and requires symbolic toolbox
+%tic
+M = sym('M','positive');
+eqn = M.^6 + 3*M*(sin(aeq)).^4 == 4*(sin(aeq)).^4;
+COS2M = vpasolve(eqn,M,0.5);
+COSM = double(sqrt(COS2M(2)));
+lm1 = acos(COSM);
+%toc
+
+lm,lm1,lm-lm1
