4dc6dabf5fc049a2ca7f5a23a2a0934a44a93d8f
patrick <patrick>
2017-11-29 15:37:38 +0000

    *** empty log message ***

--- testint.m
+++ testint.m
@@ -1,11 +1,17 @@
 function I = testint(s,c,beta)
 
+opts = {'AbsTol',1e-12,'RelTol',1e-9};
+
 lm = getLatMP(s,c,beta);
 
-I = integral(@(t)fun(t,c),-lm,lm,'AbsTol',1e-9,'RelTol',1e-6);
+I = zeros(size(beta));
+for i=1:length(beta),
+  I(i) = integral(@(t)fun(t,c),-lm(i),lm(i),opts{:});
+end
 
 function y=fun(t,c)
-y = sqrt(c.rt(t).^2+Dct(c.rt,t).^2);
+%y = sqrt(c.rt(t).^2+Dct(c.rt,t).^2);
+y = sqrt(c.rt(t).^2+c.drt(t).^2);
 end
 
 end
