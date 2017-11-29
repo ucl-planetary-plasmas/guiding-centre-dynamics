cc8974337e113468ed8ae54fe0af09d87df1d949
patrick <patrick>
2017-11-29 17:09:09 +0000

    *** empty log message ***

---
+++ Omega.m
@@ -0,0 +1,41 @@
+function I = Omega(s,c,beta)
+
+opts = {'AbsTol',1e-12,'RelTol',1e-9};
+%opts = {'AbsTol',1e-6,'RelTol',1e-3};
+
+switch lower(c.type),
+
+  case {'d','dip','dipole'},
+
+    B = s.dip.B;
+    Br = s.dip.Br;
+    Bt = s.dip.Bt;
+
+  case {'m','md','mdisc'},
+
+    B = s.md.B;
+    Br = s.md.Br;
+    Bt = s.md.Bt;
+
+  otherwise
+
+    error('magnetic field type c.type not recognised',c.type)
+
+end
+
+lm = getLatMP(s,c,beta);
+Bm = B(c.rt(lm),sin(lm));
+
+I = zeros(size(beta));
+for i=1:length(beta),
+  I(i) = integral(@(t)f(t,c,Bm(i)),0,lm(i),opts{:});
+end
+
+function y=f(t,c,Bm)
+r = c.rt(t);
+st = sin(t);
+y = (sqrt(((Br(r,st)./Bt(r,st)).^2+1)./(1-B(r,st)/Bm)).*r);
+end
+
+end
+
---
+++ Omegad.m
@@ -0,0 +1,19 @@
+function I = Omegad(r,beta)
+
+opts = {'AbsTol',1e-12,'RelTol',1e-9};
+
+lm = getLatMPd(r,beta);
+
+I = zeros(size(beta));
+for i=1:length(beta),
+  I(i) = r*integral(@(t)f(t,r,beta(i)),0,lm(i),opts{:});
+end
+
+function y=f(t,r,b)
+y = (1-sqrt(1+3*sin(t).^2)*sin(b)^2/2./cos(t).^6./ ...
+    sqrt(1-sqrt(1+3*sin(t).^2)*sin(b)^2./cos(t).^6).* ...;
+		cos(t).^3.*(sin(t).^2+1)./sqrt(1+3*sin(t).^2)./(1+3*sin(t).^2);
+end
+
+end
+
