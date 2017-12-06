80d8b1bfa53792f9e7efd5b75c397aadd53f5a5e
patrick <patrick>
2017-12-06 10:12:46 +0000

    *** empty log message ***

--- BxgradB.m
+++ BxgradB.m
@@ -9,14 +9,15 @@ bt = Bt(r,st);
 b = B(r,st);
 
 gradBr = D2r(B,r,st);
-gradBt = 1./r.*D2t(B,r,st);
+% - sign due to orientation of mu
+gradBt = -1./r.*D2t(B,r,st);
 
-% + sign due to orientation!
-BxdB = abs(br.*gradBt + bt.*gradBr);
+% B x grad B 
+BxdB = br.*gradBt - bt.*gradBr;
 
 return
 
 subplot(311), plot(t,BxdB), title('BxdB components');
 subplot(312), plot(t,br.*gradBt),
-subplot(313), plot(t,bt.*gradBr),
+subplot(313), plot(t,-bt.*gradBr),
 pause
