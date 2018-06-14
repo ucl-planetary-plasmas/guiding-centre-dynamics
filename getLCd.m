d107dca258718df6772bd7014be2c8e5ab6b2ac0
patrick <patrick>
2018-06-14 14:44:24 +0000

    Added license.

--- getLCd.m
+++ getLCd.m
@@ -1,3 +1,23 @@
 function lc = getLCd(L)
+% function lc = getLCd(L)
+
+%
+% $Id: getLCd.m,v 1.2 2018/06/14 14:46:42 patrick Exp $
+%
+% Copyright (c) 2018 Patrick Guio <patrick.guio@gmail.com>
+% All Rights Reserved.
+%
+% This program is free software; you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by the
+% Free Software Foundation; either version 3 of the License, or (at your
+% option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+% Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 lc = asin(sqrt(1./(4*L.^6-3*L.^5)));
