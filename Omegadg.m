64ea7d9bc5a9e3330b523ba09b9cc4f458b52cd2
patrick <patrick>
2018-06-14 14:52:31 +0000

    Added license.

--- Omegadg.m
+++ Omegadg.m
@@ -1,4 +1,24 @@
 function I = Omegadg(r,beta)
+% function I = Omegadg(r,beta)
+
+%
+% $Id: Omegadg.m,v 1.8 2018/06/14 14:52:31 patrick Exp $
+%
+% Copyright (c) 2018 Patrick Guio <patrick.guio@gmail.com>
+% All Rights Reserved.
+%
+% This program is free software; you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by the
+% Free Software Foundation; either version 3 of the License, or (at your
+% option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+% Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 opts = {'AbsTol',1e-12,'RelTol',1e-9};
 
--- readMdisc.m
+++ readMdisc.m
@@ -1,4 +1,24 @@
 function s=readMdisc(mdiscFile)
+% function s=readMdisc(mdiscFile)
+
+%
+% $Id: readMdisc.m,v 1.2 2018/06/14 14:54:59 patrick Exp $
+%
+% Copyright (c) 2018 Patrick Guio <patrick.guio@gmail.com>
+% All Rights Reserved.
+%
+% This program is free software; you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by the
+% Free Software Foundation; either version 3 of the License, or (at your
+% option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+% Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 s = load(mdiscFile);
 
