34295fd530cfb4fd649ae19d5aba2afa3cdf109c
patrick <patrick>
2019-07-16 14:30:02 +0000

    Added license and help.

--- getLmd.m
+++ getLmd.m
@@ -1,4 +1,24 @@
 function Lmd = getLmd(s,L)
+% function Lmd = getLmd(s,L)
+
+%
+% $Id: getLmd.m,v 1.6 2019/07/16 14:30:02 patrick Exp $
+%
+% Copyright (c) 2019 Patrick Guio <patrick.guio@gmail.com>
+% All Rights Reserved.
+%
+% This program is free software; you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by the
+% Free Software Foundation; either version 3 of the License, or (at your
+% option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+% Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 %opts = {'linear','extrap'};
 %opts = {'spline','extrap'};
