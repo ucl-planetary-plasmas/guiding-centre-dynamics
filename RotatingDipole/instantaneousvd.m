63a6a5bd0792292a0e9c9b7d692a960124606f0d
soc <saboc@vivaldi.net>
2022-06-27 09:05:51 +0200

    instantaneousvd.m: last update

--- RotatingDipole/instantaneousvd.m
+++ RotatingDipole/instantaneousvd.m
@@ -12,7 +12,7 @@ L = linspace(5, 10, nL);
 c.distrib.L = L;
 c.distrib.subcorot = 0.8; %=1 == rigid rotation
 ec86 = caudal86cold(L);
-c.distrib.E0 = ec86;
+c.distrib.E0 = (3/2).*ec86;
 % eb94te = bagenal94thelec(L);
 % c.distrib.E0 = eb94te;
 a0mx = pi/2-1e-1;
@@ -25,7 +25,7 @@ b0 = computebeta0(c);
 al = computealphaloss(b0, c.distrib.L);
 [ints, alpha0] = integrals(al, b0,c.distrib.a0(1), c.distrib.a0(2));
 
-nl = 60; l = linspace(0, pi/2, nl);% latitude \lambda
+nl = 1e2; l = linspace(0, pi/2, nl);% latitude \lambda
 
 % instantaneaous velocities at the equator
 vd0s = driftvelocities(c, alpha0, b0);
@@ -48,6 +48,7 @@ a0 = reshape(alpha0(1,:,:), [nL,na0]);
 vd0 = reshape(vd0s.vd0(1, :, :), [nL,na0]);
 vd0cg = reshape(vd0s.vd0cg(1, :, :), [nL,na0]);
 vd0cf = vd0s.vd0cf;
+lm = reshape(ints.lm(1,:,:), [nL,na0]);
 %% Intantaneous drift velocities 
 % 2D plots
 vdscgL5 = vd0cg(1, :)'*cgl;
@@ -70,8 +71,8 @@ colormap(jet(50));
 cb=colorbar;
 hold off
 
-vdla00 = vd0cg(:, 1)*cgl+vd0cfl;
-vdla0pi2 = vd0cg(:, end)*cgl+vd0cfl;
+vdla00 = vd0cg(:, 1)*cgl+vdscfl;
+vdla0pi2 = vd0cg(:, end)*cgl+vdscfl;
 figure('name', 'vds wrt lat and L')
 hold on
 % pcolor(vdla00)
@@ -113,3 +114,22 @@ xlabel('Latitude \lambda [ ]')
 legend
 grid on
 hold off
+
+% CF vs CG term (dimensionless)
+figure('name','CF vs CG term)')
+hold on
+plot(l, (3/2).*cgl, 'displayname', 'CG min : (3/2)cgl')
+plot(l, 3.*cgl, 'displayname', 'CG max : 3 cgl')
+plot(l, min(b0).*cfl, 'displayname', ['CF min : ', num2str(min(b0)), ' cfl'])
+plot(l, max(b0).*cfl, 'displayname', ['CF max : ', num2str(max(b0)), ' cfl'])
+plot([min(lm(:, 1)) min(lm(:,1))], [-5 160], 'displayname', 'min lm, a0 = 0')
+plot([min(lm(:, end)) min(lm(:,end))],[-5 160], 'displayname', 'min lm, a0 = pi/2')
+plot([max(lm(:, 1)) max(lm(:,1))], [-5 160], 'displayname', 'max lm, a0 = 0')
+plot([max(lm(:, end)) max(lm(:,end))],[-5 160], 'displayname', 'max lm, a0 = pi/2')
+hold off
+xlabel('Latitude \lambda [ ]')
+ylim([-5, 160])
+legend
+grid on
+hold off
+
