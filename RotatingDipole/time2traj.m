2a473a0b33456cf27866be261d4a8f4ddb1acdb3
soc <saboc@vivaldi.net>
2022-06-27 14:24:03 +0200

    time2traj : unsuccesfull attempt to compare also with the trajectory data from the file Spacecraft_trajectory_vip4_can.csv

--- RotatingDipole/time2traj.m
+++ RotatingDipole/time2traj.m
@@ -1,4 +1,4 @@
-older = 'D:\saboc\Documents\EPFL\PDM-UiT\Bagenal_DataBoulder\Voyager\';
+folder = 'D:\saboc\Documents\EPFL\PDM-UiT\Bagenal_DataBoulder\Voyager\';
 
 %% V1
 numVars = 7;
@@ -30,10 +30,49 @@ rho64 = rhospin(i64);
 % plot(hh64, rho64)
 % plot(rho64, hh64)
 
+% %% Position from VIP4_CAN %%%PBMs
+% filename = [folder,'Spacecraft_trajectory_vip4_can.xlsx'];
+% 
+% numVars = 7;
+% varNames = {'Time', 'Radius_sc', 'Theta_sc', ...
+%     'Radius_mag', 'Theta_mag', 'Radius_Cent, Theta_cent'};
+% dataStartLoc = 'A2';
+% varTypes = {'double', 'double', 'double', 'double', ...
+%     'double', 'double'};%%, 'double'};
+% opt = spreadsheetImportOptions('NumVariables',numVars,...
+%                                 'VariableNames',varNames,...
+%                                 'VariableTypes', varTypes, ...
+%                                 'DataRange', dataStartLoc); 
+% data = readmatrix(filename,opt);
+% day = fix(data(:,1));
+% hours = (data(:,1) -day).*24;
+% 
+% rho_sc = data(:, 2).*cosd(data(:,3));
+% rho_mag = data(:, 4).*cosd(data(:,5));
+% % rho_cent = data(:,6).*cosd(data(:,7));
+% 
+% j64 = find(day==64);
+% 
+% %Compa %%STRANGE !
+% figure
+% hold on
+% plot(hh64, rho64, 'k-')
+% plot(hours(j64), rho_sc(j64))
+% plot(hours(j64), rho_mag(j64))
+% plot(hours(j64), rho_cent(j64))
+% xlabel('time')
+% ylabel('r')
+% grid on
+% hold off
+
 %% Time points of electrons properties
 filename = [folder,'electron_properties.xlsx'];
 time = readmatrix(filename, 'Range','A2:A242');
 
 %Interpolation
 rho = interp1(hh64,rho64,time);
-plot(rho, time, '+-');
+
+figure
+hold on
+plot(rho64, hh64, 'k-')
+plot(rho, time, '+')
